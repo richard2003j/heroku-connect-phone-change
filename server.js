44e79a1054841c13196157d5d0e75899d1631905
richard2003j <40558836+richard2003j@users.noreply.github.com>
2020-03-12 18:11:11 +0900

    t1

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.set('port', process.env.PORT || 5000);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-var router = app.Router();
-router.post('/update', function(req, res) {
+
+app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
@@ -41,9 +41,6 @@ router.post('/update', function(req, res) {
     });
 });
 
-router.post('/init',function (err, conn, done){
-   res.json({error: "test init"}); 
-});
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
