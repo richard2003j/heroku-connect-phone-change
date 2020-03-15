22b7bf9fcdf4e9b7de161e772a578695ec652a80
richard2003j <40558836+richard2003j@users.noreply.github.com>
2020-03-15 13:39:12 +0900

    accout ok

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.post('/update', function(req, res) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone =WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone =$1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
             [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
@@ -28,6 +28,7 @@ app.post('/update', function(req, res) {
                     else {
                         // this will still cause jquery to display 'Record updated!'
                         // eventhough it was inserted
+						console.log(result);
                         res.json(result);
                     }
                   });
@@ -42,13 +43,17 @@ app.post('/update', function(req, res) {
 });
             
 
-app.get('/init-act',function (req, res){
+app.get('/account-init',function (req, res){
     
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
+		var qrystr = 'Select Name,TorihikisakiNo__c, HerokuId__c,Tantoshashimei__c, ' + 
+		'Denwabango__c, Emaiil1__c, Emaiil2__c, Emaiil3__c ' + 
+		'	from salesforce.Account ';
+
         conn.query(
-			'Select Name,TorihikisakiNo__c,Tantoshashimei__c,Denwabango__c, Emaiil1__c, Emaiil2__c, Emaiil3__c from salesforce.Account ',
+			qrystr,
 			[],
             function(err, result) {
                 if (err != null ) {
@@ -62,12 +67,94 @@ app.get('/init-act',function (req, res){
         );
     });
 });
-app.post('/init-act-edit',function (req, res){
-    
-    console.log('action : ' + req.body.action);
-	console.log('data name : ' + req.body.data[0].name);
-	res.json({data: req.data});
+app.get('/account-create-edit',function (req, res){
+     console.log("uri:account-create-edit");
+	 var idSyc = Object.keys(req.query.data)[0];
+	 var upObj = req.query.data[idSyc];
+	 var herokuId = upObj.herokuid__c;
+	 var action = req.query.action;
+
+    console.log("id: " +idSyc);
+    console.log("herokuId: " +herokuId);
+    console.log("object:" );
+    console.log( upObj);
+    console.log("action:" + req.query.action);
+
+	//insert qry
+	if( action == "create" && herokuId =='' ){
+		res.status(400).json({error: '新規作成でHerokuIDが必須です。'});
+		return;
+	}
+	inststr = 'INSERT INTO salesforce.Account( Name, HerokuId__c, ' +
+			' Tantoshashimei__c,  ' +
+			' Denwabango__c, ' +
+			' Emaiil1__c , ' +
+			' Emaiil2__c, ' +
+			' Emaiil3__c) ' +
+			' VALUES($1,$2,$3,$4,$5,$6,$7) ' ;
+	instvar =[upObj.name,
+			herokuId,
+			upObj.tantoshashimei__c,
+			upObj.denwabango__c,
+			upObj.emaiil1__c,upObj.emaiil2__c,upObj.emaiil3__c,
+			];
+	//update qry
+	updstr = 'Update salesforce.Account SET Name = $1, HerokuId__c = $2, ' +
+			' Tantoshashimei__c = $3, ' +
+			' Denwabango__c = $4, ' +
+			' Emaiil1__c = $5, ' +
+			' Emaiil2__c = $6, ' +
+			' Emaiil3__c = $7 ' +
+			' WHERE  TorihikisakiNo__c= $8 ' ;
+	updvar =[upObj.name,
+			herokuId,
+			upObj.tantoshashimei__c,
+			upObj.denwabango__c,
+			upObj.emaiil1__c,upObj.emaiil2__c,upObj.emaiil3__c,
+			idSyc];
+	//return qry
+	var qrystr = 'Select Name,TorihikisakiNo__c, HerokuId__c,Tantoshashimei__c, ' + 
+		'Denwabango__c, Emaiil1__c, Emaiil2__c, Emaiil3__c ' + 
+		'	from salesforce.Account '+
+		' where TorihikisakiNo__c= $1 or HerokuId__c = $2';
+	
+	if(action == "create"){
+		updstr = inststr;
+		updvar = instvar;
+	}
+
+    console.log( updstr);
+
+	pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+			updstr,
+			updvar,
+            function(err, result) {
+                if (err != null ) {
+					res.status(400).json({error: err.message});
+                }
+                else {
+					conn.query(
+						qrystr,
+						[idSyc,herokuId],
+						function(err, result) {
+							if (err != null ) {
+								res.status(400).json({error: err.message});
+							}
+							else {
+								done();
+								res.json({data: result.rows});
+							}
+						}
+					);
+                }
+            }
+        );
+    });
 });
+
 app.get('/',function (req, res){
    res.sendFile('./public/simple-tbl.html'); 
 });
