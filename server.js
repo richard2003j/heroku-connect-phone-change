f3e9159573443278b5a086f4007847a995c36d2f
richard2003j <40558836+richard2003j@users.noreply.github.com>
2020-03-16 18:45:34 +0900

    edit

--- server.js
+++ server.js
@@ -57,14 +57,14 @@ app.get('/account-init',function (req, res){
 app.get('/account-create-edit',function (req, res){
      console.log("uri:account-create-edit");
 	 var idSyc = Object.keys(req.query.data)[0];
-	 var upObj = req.query.data[idSyc];
-	 var herokuId = upObj.herokuid__c;
+	 var upInsO = req.query.data[idSyc];
+	 var herokuId = upInsO.herokuid__c;
 	 var action = req.query.action;
 
     console.log("id: " +idSyc);
     console.log("herokuId: " +herokuId);
     console.log("object:" );
-    console.log( upObj);
+    console.log( upInsO);
     console.log("action:" + req.query.action);
 
 	//insert qry
@@ -72,42 +72,137 @@ app.get('/account-create-edit',function (req, res){
 		res.status(400).json({error: '新規作成でHerokuIDが必須です。'});
 		return;
 	}
-	inststr = 'INSERT INTO salesforce.Account( Name, HerokuId__c, ' +
-			' KakuninStatus__c,  ' +
-			' Tantoshashimei__c,  ' +
-			' Denwabango__c, ' +
-			' Emaiil1__c , ' +
-			' Emaiil2__c, ' +
-			' Emaiil3__c) ' +
-			' VALUES($1,$2,$3,$4,$5,$6,$7,$8) ' ;
-	instvar =[upObj.name,
-			herokuId,
-			upObj.kakuninstatus__c,
-			upObj.tantoshashimei__c,
-			upObj.denwabango__c,
-			upObj.emaiil1__c,upObj.emaiil2__c,upObj.emaiil3__c,
+	inststr = 'INSERT INTO salesforce.Account( ' + 
+						'Name ,' + 
+						'Eibuntorihikisakimei__c ,' + 
+						'Kakogaishamei__c ,' + 
+						'Kuni__c ,' + 
+						'Yuubimbango__c ,' + 
+						'Jusho1__c ,' + 
+						'Jusho2__c ,' + 
+						'Daihyoshayakushoku__c ,' + 
+						'Daihyoshashimei__c ,' + 
+						'Busho__c ,' + 
+						'Tantoshashimei__c ,' + 
+						'Denwabango__c ,' + 
+						'Emaiil1__c ,' + 
+						'Emaiil2__c ,' + 
+						'Emaiil3__c ,' + 
+						'KakuninStatus__c ,' + 
+						'KeiyakuStatus__c ,' + 
+						'Shinseistatus__c ,' + 
+						'HerokuId__c ' + 
+			') ' +
+			' VALUES( ' + 
+					 ' $1, ' +
+					 ' $2, ' +
+					 ' $3, ' +
+					 ' $4, ' +
+					 ' $5, ' +
+					 ' $6, ' +
+					 ' $7, ' +
+					 ' $8, ' +
+					 ' $9, ' +
+					 ' $10, ' +
+					 ' $11, ' +
+					 ' $12, ' +
+					 ' $13, ' +
+					 ' $14, ' +
+					 ' $15, ' +
+					 ' $16, ' +
+					 ' $17, ' +
+					 ' $18, ' +
+					 ' $19 ' +
+			') ' ;
+	instvar =[
+					upInsO.name,
+					upInsO.eibuntorihikisakimei__c,
+					upInsO.kakogaishamei__c,
+					upInsO.kuni__c,
+					upInsO.yuubimbango__c,
+					upInsO.jusho1__c,
+					upInsO.jusho2__c,
+					upInsO.daihyoshayakushoku__c,
+					upInsO.daihyoshashimei__c,
+					upInsO.busho__c,
+					upInsO.tantoshashimei__c,
+					upInsO.denwabango__c,
+					upInsO.emaiil1__c,
+					upInsO.emaiil2__c,
+					upInsO.emaiil3__c,
+					upInsO.kakuninstatus__c,
+					upInsO.keiyakustatus__c,
+					upInsO.shinseistatus__c,
+					upInsO.herokuid__c
 			];
 	//update qry
-	updstr = 'Update salesforce.Account SET Name = $1, HerokuId__c = $2, ' +
-			' KakuninStatus__c = $3, ' +
-			' Tantoshashimei__c = $4, ' +
-			' Denwabango__c = $5, ' +
-			' Emaiil1__c = $6, ' +
-			' Emaiil2__c = $7, ' +
-			' Emaiil3__c = $8 ' +
-			' WHERE  TorihikisakiNo__c= $9 ' ;
-	updvar =[upObj.name,
-			herokuId,
-			upObj.kakuninstatus__c,
-			upObj.tantoshashimei__c,
-			upObj.denwabango__c,
-			upObj.emaiil1__c,upObj.emaiil2__c,upObj.emaiil3__c,
-			idSyc];
+	updstr = 'Update salesforce.Account SET '+
+					'Name= $1  ,'  +
+					'Eibuntorihikisakimei__c= $2  ,'  +
+					'Kakogaishamei__c= $3  ,'  +
+					'Kuni__c= $4  ,'  +
+					'Yuubimbango__c= $5  ,'  +
+					'Jusho1__c= $6  ,'  +
+					'Jusho2__c= $7  ,'  +
+					'Daihyoshayakushoku__c= $8  ,'  +
+					'Daihyoshashimei__c= $9  ,'  +
+					'Busho__c= $10  ,'  +
+					'Tantoshashimei__c= $11  ,'  +
+					'Denwabango__c= $12  ,'  +
+					'Emaiil1__c= $13  ,'  +
+					'Emaiil2__c= $14  ,'  +
+					'Emaiil3__c= $15  ,'  +
+					'KakuninStatus__c= $16  ,'  +
+					'KeiyakuStatus__c= $17  ,'  +
+					'Shinseistatus__c= $18  ,'  +
+					'HerokuId__c= $19  '  +
+			' WHERE  TorihikisakiNo__c= $20 ' ;
+	updvar =[
+					upInsO.name,
+					upInsO.eibuntorihikisakimei__c,
+					upInsO.kakogaishamei__c,
+					upInsO.kuni__c,
+					upInsO.yuubimbango__c,
+					upInsO.jusho1__c,
+					upInsO.jusho2__c,
+					upInsO.daihyoshayakushoku__c,
+					upInsO.daihyoshashimei__c,
+					upInsO.busho__c,
+					upInsO.tantoshashimei__c,
+					upInsO.denwabango__c,
+					upInsO.emaiil1__c,
+					upInsO.emaiil2__c,
+					upInsO.emaiil3__c,
+					upInsO.kakuninstatus__c,
+					upInsO.keiyakustatus__c,
+					upInsO.shinseistatus__c,
+					upInsO.herokuid__c,
+					upInsO.torihikisakino__c
+			];
 	//return qry
-	var qrystr = 'Select Name,TorihikisakiNo__c,KakuninStatus__c, HerokuId__c,Tantoshashimei__c, ' + 
-		'Denwabango__c, Emaiil1__c, Emaiil2__c, Emaiil3__c ' + 
-		'	from salesforce.Account '+
-		' where TorihikisakiNo__c= $1 ';
+	var qrystr = 'Select ' +
+							'Name ,' + 
+							'TorihikisakiNo__c ,' + 
+							'Eibuntorihikisakimei__c ,' + 
+							'Kakogaishamei__c ,' + 
+							'Kuni__c ,' + 
+							'Yuubimbango__c ,' + 
+							'Jusho1__c ,' + 
+							'Jusho2__c ,' + 
+							'Daihyoshayakushoku__c ,' + 
+							'Daihyoshashimei__c ,' + 
+							'Busho__c ,' + 
+							'Tantoshashimei__c ,' + 
+							'Denwabango__c ,' + 
+							'Emaiil1__c ,' + 
+							'Emaiil2__c ,' + 
+							'Emaiil3__c ,' + 
+							'KakuninStatus__c ,' + 
+							'KeiyakuStatus__c ,' + 
+							'Shinseistatus__c ,' + 
+							'HerokuId__c ' + 
+				' from salesforce.Account '+
+				' where TorihikisakiNo__c= $1 ';
 	
 	if(action == "create"){
 		updstr = inststr;
@@ -144,10 +239,6 @@ app.get('/account-create-edit',function (req, res){
             }
         );
     });
-});
-
-app.get('/',function (req, res){
-   res.sendFile('./public/simple-tbl.html'); 
 });
 
 app.listen(app.get('port'), function () {
