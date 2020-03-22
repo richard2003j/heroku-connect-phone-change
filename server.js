e2c096861a7e97da5257e80dd1decf8f09f471cd
richard2003j <40558836+richard2003j@users.noreply.github.com>
2020-03-22 14:47:54 +0900

    modify

--- server.js
+++ server.js
@@ -11,13 +11,25 @@ app.use(bodyParser.json());
 // common sfid, name
 const CommonListCtl = require('./controller/common-sfidname');
 app.get('/common-init',CommonListCtl);
-// contact list
+// tmi-userid-crud
 // account list
 const AccountListCtl = require('./controller/account-server');
 app.get('/account-crud',AccountListCtl);
 // contact list
 const ContactListCtl = require('./controller/contact-server');
 app.get('/contact-crud',ContactListCtl);
+// tmi-kihonkeiyaku list
+const TmiKihonkeiyakuListCtl = require('./controller/tmi-kihonkeiyaku-server');
+app.get('/tmi-kihonkeiyaku-crud',TmiKihonkeiyakuListCtl);
+// tmi-keiyakuapi list
+const TmiKeiyakuapiListCtl = require('./controller/tmi-keiyakuapi-server');
+app.get('/tmi-keiyakuapi-crud',TmiKeiyakuapiListCtl);
+// tmi-accesskey list
+const TmiAccesskeyListCtl = require('./controller/tmi-accesskey-server');
+app.get('/tmi-accesskey-crud',TmiAccesskeyListCtl);
+// tmi-userid list
+const TmiUseridListCtl = require('./controller/tmi-userid-server');
+app.get('/tmi-userid-crud',TmiUseridListCtl);
 
 
 app.listen(app.get('port'), function () {
