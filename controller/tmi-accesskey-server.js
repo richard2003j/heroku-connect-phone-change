e4522eecba445b99a6c6595332540cb8bcd1f74b
rivhard2003j <ec2-user@ip-10-0-2-248.ap-northeast-1.compute.internal>
2020-03-23 09:55:46 +0000

    modify

--- controller/tmi-accesskey-server.js
+++ controller/tmi-accesskey-server.js
@@ -76,6 +76,12 @@ module.exports = function (req, res){
 				];
 	}
 	//return qry
+	var log_msg = ' (select record_id, table_name, sf_message from salesforce._trigger_log ' +
+		' where (record_id, table_name, processed_at) in ' + 
+		'(select record_id, table_name,  max(processed_at) as processed_at  from salesforce._trigger_log ' + 
+		' where table_name = $1 ';
+		' group by record_id, table_name) ) b ';
+	
 	var qrystr = 'Select ' +
 						'Name ,' + 
 						'KihonkeiyakuNo__c ,' + 
@@ -87,22 +93,25 @@ module.exports = function (req, res){
 						'Henkoriyu__c ,' + 
 						'HerokuId__c, ' + 
 						'sf_message ' + 
-					' from salesforce.TMI_Accesskey_API__c a left join  salesforce._trigger_log b on a.sfid = b.sfid  ';
-	var qryVar = [idSyc];
+					' from salesforce.TMI_Accesskey_API__c a left join  ' + log_msg +  ' on a.id = b.record_id  ';
+	
+	
+	var qryVar = ['tmi_accesskey_api__c'];
 
 	// dispather
 	if(action == "create"){
 		updstr = inststr;
 		updvar = instvar;
-		qrystr += ' and HerokuId__c= $1 ';
-		qryVar = [herokuId];
+		qrystr += ' and HerokuId__c= $2 ';
+		qryVar = qryVar.concat([herokuId]);
 	}else if(action == "edit"){
-		qrystr += ' and Name = $1 ';
+		qrystr += ' and Name = $2 ';
+		qryVar = qryVar.concat([idSyc]);
 	}else{
 		updstr = "select count(id) from salesforce.Contact ";
 		updvar = [];
 		qrystr = qrystr;
-		qryVar = [];
+		qryVar = qryVar;
 	}
 
     console.log( updstr);
