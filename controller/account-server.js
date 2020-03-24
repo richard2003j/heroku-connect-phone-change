679ecea60d32fa169027e05cf515bae7ea1b4b32
rivhard2003j <ec2-user@ip-10-0-2-248.ap-northeast-1.compute.internal>
2020-03-24 01:05:28 +0000

    mod account

--- controller/account-server.js
+++ controller/account-server.js
@@ -130,7 +130,16 @@ module.exports = function (req, res){
 						idSyc
 				];
 	}
+	
+	
 	//return qry
+	var log_msg = ' (select record_id, table_name, sf_message from salesforce._trigger_log ' +
+		' where (record_id, table_name, processed_at) in ' + 
+		'(select record_id, table_name,  max(processed_at) as processed_at  from salesforce._trigger_log ' + 
+		' where table_name = $1 ' +
+		' group by record_id, table_name) ) b ';
+
+
 	var qrystr = 'Select ' +
 					'Name ,' + 
 					'TorihikisakiNo__c ,' + 
@@ -151,23 +160,26 @@ module.exports = function (req, res){
 					'KakuninStatus__c ,' + 
 					'KeiyakuStatus__c ,' + 
 					'Shinseistatus__c ,' + 
-					'HerokuId__c ' + 
-				' from salesforce.Account ';
-	var qryVar = [idSyc];
+					'HerokuId__c , ' + 
+					'sf_message ' + 
+				' from salesforce.Account a left join  ' + log_msg +  ' on a.id = b.record_id  ';
+
+	var qryVar = ['account'];
 
 	// dispather
 	if(action == "create"){
 		updstr = inststr;
 		updvar = instvar;
-		qrystr += ' where HerokuId__c= $1 ';
-		qryVar = [herokuId];
+		qrystr += ' and HerokuId__c= $2 ';
+		qryVar = qryVar.concat([herokuId]);
 	}else if(action == "edit"){
-		qrystr += ' where TorihikisakiNo__c= $1 ';
+		qrystr += ' and Name = $2 ';
+		qryVar = qryVar.concat([idSyc]);
 	}else{
 		updstr = "select count(id) from salesforce.Account ";
 		updvar = [];
 		qrystr = qrystr;
-		qryVar = [];
+		qryVar = qryVar;
 	}
 
     console.log( updstr);
